2ae74d292f7c0a1b42c7781643b8750e33ae3bb8
youngseaz <1343500021@qq.com>
2020-01-31 13:20:24 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name="DnsPod",
+    version="1.0.0",
+    description="ddns powerby DnsPod",
+    author="youngseaz",
+    author_email="email@youngseaz.com",
+    url="https://github.com/youngseaz/dnspod.git",
+    install_requires=["pycryptodome"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ]
+)
